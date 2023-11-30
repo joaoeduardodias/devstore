646686c27f4d415eda66fa86402684a5bfde51b1
joaoeduardodias <joaoeduardodias123@gmail.com>
2023-11-30 15:14:15 -0300

    fea: create page product

--- src/app/api/products/featured/route.ts
+++ src/app/api/products/featured/route.ts
@@ -1,6 +1,7 @@
 import data from '../data.json'
 
-export function GET() {
+export async function GET() {
+  await new Promise((resolve) => setTimeout(resolve, 1000))
   const featuredProducts = data.products.filter(
     (product) => product.featured === true,
   )
